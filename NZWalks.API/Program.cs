f4a94b85a842f4b0929dab26235c5c4c4828318d
Wes Benvindo <wal2.designer@gmail.com>
2024-12-29 18:48:13 +0000

    Injecting DbContext

--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+//Injecting the db context class
+builder.Services.AddDbContext<NZWalksDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("NZWalksConnectionString")));
 
 var app = builder.Build();
 
